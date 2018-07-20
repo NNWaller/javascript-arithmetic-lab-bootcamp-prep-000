0f25b4210e9d6a156b704dad99fdb289f2d85491
Natasha Waller <nnwaller@gmail.com>
2018-07-20 01:48:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add (){
+  return a + b
+}
+
+function subtract (){
+  return a - b
+}
+
+function multiply (){
+  return a * b
+}
+
+function divide (){
+  return a / b
+}
+
+function inc (){
+  return n++
+}
+
+function dec (){
+  return n--
+}
+
+function makeInt (){
+  
+}
